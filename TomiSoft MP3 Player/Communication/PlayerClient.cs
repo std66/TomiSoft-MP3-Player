d4f575a9c18a0629bac3d6a01e9ceed4d74a6c9d
std66 <sinkutamas@gmail.com>
2016-12-03 21:02:05 +0100

    Documented PlayerClient class.

--- TomiSoft MP3 Player/Communication/PlayerClient.cs
+++ TomiSoft MP3 Player/Communication/PlayerClient.cs
@@ -3,24 +3,36 @@ using System.IO;
 using System.Net.Sockets;
 
 namespace TomiSoft.MP3Player.Communication {
+	/// <summary>
+	/// This class represents a client connection to the server. Use
+	/// this class to send specific commands to the already running
+	/// instance.
+	/// </summary>
 	public class PlayerClient : IDisposable {
 		private TcpClient Client;
 		private StreamWriter sw;
 		private StreamReader sr;
 
-		public bool ServerRunning {
+		/// <summary>
+		/// Gets whether the server is ready to accept commands.
+		/// </summary>
+		public bool ServerReady {
 			get {
 				this.sw.WriteLine("IsRunning;");
 				return (this.sr.ReadLine().Contains("true"));
 			}
 		}
 
+		/// <summary>
+		/// Determines whether the server is running.
+		/// </summary>
+		/// <returns>True if the server is running, false if not.</returns>
 		public static bool IsServerRunning() {
 			bool Result = false;
 
 			try {
 				using (PlayerClient c = new PlayerClient()) {
-					Result = c.ServerRunning;
+					Result = c.ServerReady;
 				}
 			}
 			catch (SocketException) {
@@ -30,6 +42,10 @@ namespace TomiSoft.MP3Player.Communication {
 			return Result;
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the PlayerClient class. Connets to the already
+		/// running instance.
+		/// </summary>
 		public PlayerClient() {
 			this.Client = new TcpClient("localhost", 22613);
 			this.sw = new StreamWriter(this.Client.GetStream()) {
@@ -38,10 +54,17 @@ namespace TomiSoft.MP3Player.Communication {
 			this.sr = new StreamReader(this.Client.GetStream());
 		}
 
+		/// <summary>
+		/// Sends the command to the server to play the given file.
+		/// </summary>
+		/// <param name="Filename">The file's full path to play</param>
 		public void Play(string Filename) {
 			this.sw.WriteLine(String.Format("Play;{0}", Filename));
 		}
 
+		/// <summary>
+		/// Closes the connection to the server.
+		/// </summary>
 		public void Dispose() {
 			this.Client.Close();
 		}
