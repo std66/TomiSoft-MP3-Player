5ea795012b362f7458be83b3763187f34d7c4fcf
std66 <sinkutamas@gmail.com>
2017-03-19 22:26:01 +0100

    Initial implementation of HeadRequest.

---
+++ TomiSoft.Web.Http/HeadRequest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomiSoft.Web.Http {
+	public class HeadRequest {
+		private uint maxRedirects;
+
+		public uint MaxRedirects {
+			get { return maxRedirects; }
+			set {
+				maxRedirects = value;
+			}
+		}
+		
+		public bool AllowRedirects {
+			get;
+			set;
+		}
+
+		public async Task<HeadWebResponse> SendRequest(string Location) {
+			return await this.SendRequest(new Uri(Location));
+		}
+
+		public async Task<HeadWebResponse> SendRequest(Uri Location) {
+			HeadWebResponse Result = null;
+			uint Redirects = 0;
+
+			do {
+				TcpClient Client = new TcpClient(Location.Host, Location.Port);
+				using (StreamWriter sw = new StreamWriter(Client.GetStream())) {
+					StreamReader sr = new StreamReader(Client.GetStream());
+
+					await sw.WriteAsync(this.PrepareRequest(Location));
+
+					StringBuilder sb = new StringBuilder();
+					string CurrentLine;
+					do {
+						CurrentLine = await sr.ReadLineAsync();
+						sb.AppendLine(CurrentLine);
+					}
+					while (!String.IsNullOrWhiteSpace(CurrentLine));
+
+					Result = HeadWebResponse.Parse(sb.ToString());
+
+					if (Result.IsRedirect) {
+						Redirects++;
+						
+					}
+				}
+			}
+			while (Result.IsRedirect && Redirects < this.maxRedirects && this.AllowRedirects);
+
+			return Result;
+		}
+
+		private string PrepareRequest(Uri u) {
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"HEAD {u.PathAndQuery} HTTP/1.0");
+			sb.AppendLine($"User-Agent: TomiSoft.Web.Http.HeadRequest ({Assembly.GetExecutingAssembly().ToString()})");
+			sb.AppendLine();
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ TomiSoft.Web.Http/HeadWebResponse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TomiSoft.Web.Http {
+	public class HeadWebResponse : WebResponse {
+		private uint statusCode;
+
+		public uint StatusCode {
+			get { return statusCode; }
+		}
+		
+		public bool IsRedirect {
+			get {
+				return this.statusCode >= 300 && this.statusCode <= 399;
+			}
+		}
+
+		private HeadWebResponse(uint StatusCode) {
+			this.statusCode = StatusCode;
+		}
+
+		public static HeadWebResponse Parse(string Response) {
+			Regex r = new Regex(@"\r?\n");
+			string[] Lines = r.Split(Response);
+			
+			HeadWebResponse Result = new HeadWebResponse(
+				StatusCode: Convert.ToUInt32(Lines[0].Split(" ".ToCharArray(), 3).ElementAt(1))
+			);
+
+			for (int i = 1; i < Lines.Length; i++) {
+				string[] Parts = Lines[i].Split(':');
+				
+			}
+
+			return Result;
+		}
+	}
+}
