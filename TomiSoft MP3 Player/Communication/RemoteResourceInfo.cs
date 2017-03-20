04aa700d1c8afc20bbd31b026dff962e37ff5b4f
std66 <sinkutamas@gmail.com>
2017-03-20 19:50:00 +0100

    Removed TomiSoft.Web.Http, added RemoreResourceInfo.

---
+++ TomiSoft MP3 Player/Communication/RemoteResourceInfo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace TomiSoft.MP3Player.Communication {
+	/// <summary>
+	/// This class provides informations about a remote media resource.
+	/// </summary>
+	public class RemoteResourceInfo {
+		/// <summary>
+		/// Gets if the remote resource is an internet radio stream.
+		/// </summary>
+		public bool IsInternetRadioStream {
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets if the request was successful or not.
+		/// </summary>
+		public bool RequestSucceeded {
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets the length of the resource in bytes. Null is returned when
+		/// the length of the resource is unknown.
+		/// </summary>
+		public long? Length {
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RemoteResourceInfo"/> class.
+		/// </summary>
+		private RemoteResourceInfo() {}
+
+		/// <summary>
+		/// Determines the type of the given resource asynchronously that is represented
+		/// with it's URI.
+		/// </summary>
+		/// <param name="Uri">The URI that locates the remote resource.</param>
+		/// <returns>A <see cref="RemoteResourceInfo"/> instance that holds informations about the remote resource.</returns>
+		public async static Task<RemoteResourceInfo> GetUriInfoAsync(string Uri) {
+			#region Error checking
+			if (String.IsNullOrWhiteSpace(Uri))
+				throw new ArgumentException($"{nameof(Uri)} cannot be null or empty string.");
+			#endregion
+			
+			RemoteResourceInfo Result = new RemoteResourceInfo();
+			
+			HttpRequestMessage Message = new HttpRequestMessage(HttpMethod.Get, new System.Uri(Uri));
+			Task<HttpResponseMessage> RequestTask = (new HttpClient()).SendAsync(Message, HttpCompletionOption.ResponseHeadersRead);
+			HttpResponseMessage Response = await RequestTask;
+
+			Result.RequestSucceeded = Response.IsSuccessStatusCode && !RequestTask.IsFaulted;
+
+			if (Result.RequestSucceeded) {
+				Result.IsInternetRadioStream = Response.Headers.Where(x => x.Key.ToLower().StartsWith("icy-")).Any();
+				Result.Length = Response.Content?.Headers.ContentLength;
+			}
+
+			return Result;
+		}
+	}
+}
