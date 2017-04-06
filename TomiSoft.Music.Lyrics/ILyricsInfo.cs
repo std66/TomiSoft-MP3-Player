e03b6c4651360bbd4bccf0599c03cdc37b016422
std66 <sinkutamas@gmail.com>
2017-04-06 21:25:15 +0200

    Initial implementation of LrcWriter.

---
+++ TomiSoft.Music.Lyrics/ILyricsInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomiSoft.Music.Lyrics {
+	/// <summary>
+	/// Represents a lyrics handler.
+	/// </summary>
+	public interface ILyricsHandler {
+		/// <summary>
+		/// Gets if the used file format supports multiple translations.
+		/// </summary>
+		bool SupportsMultipleTranslations { get; }
+
+		/// <summary>
+		/// Gets the album of the song.
+		/// </summary>
+		string Album { get; }
+
+		/// <summary>
+		/// Gets the artist of the song.
+		/// </summary>
+		string Artist { get; }
+
+		/// <summary>
+		/// Gets the default translation ID.
+		/// </summary>
+		string DefaultTranslationID { get; }
+
+		/// <summary>
+		/// Gets the length of the song that this lyrics fits for.
+		/// </summary>
+		double Length { get; }
+
+		/// <summary>
+		/// Gets the title of the song.
+		/// </summary>
+		string Title { get; }
+
+		/// <summary>
+		/// Gets or sets the currently used translation ID.
+		/// </summary>
+		string TranslationID { get; set; }
+
+		/// <summary>
+		/// Gets the translations supported by the lyrics file.
+		/// </summary>
+		IReadOnlyDictionary<string, string> Translations { get; }
+	}
+}
---
+++ TomiSoft.Music.Lyrics/Lrc/LrcWriter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomiSoft.Music.Lyrics.Lrc {
+	public class LrcWriter : ILyricsWriter {
+		public string Album {
+			get;
+			set;
+		}
+
+		public string Artist {
+			get;
+			set;
+		}
+
+		public string DefaultTranslationID {
+			get {
+				return "default_translation";
+			}
+			set {
+
+			}
+		}
+
+		public bool SupportsMultipleTranslations {
+			get {
+				return false;
+			}
+		}
+
+		public string Title {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Creates a new instance of the <see cref="LrcWriter"/> class.
+		/// Invoking this will cause <see cref="NotImplementedException"/>
+		/// </summary>
+		public LrcWriter() {
+			throw new NotImplementedException();
+		}
+
+		public IReadOnlyDictionary<string, string> Translations {
+			get {
+				return new Dictionary<string, string>() {
+					{ "default_translation", "Default translation" }
+				};
+			}
+		}
+
+		public void AddLine(string TranslationID, ILyricsLine Line) {
+			throw new NotImplementedException();
+		}
+
+		public void AddLine(string TranslationID, double StartTime, double EndTime, string Text) {
+			throw new NotImplementedException();
+		}
+
+		public void AddTranslation(string Language) {
+			throw new NotImplementedException();
+		}
+
+		public string Build() {
+			throw new NotImplementedException();
+		}
+	}
+}
