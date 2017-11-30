b97b8c86761b5ca756827e0cae2e29653a6ff4bd
std66 <sinkutamas@gmail.com>
2017-11-30 19:12:30 +0100

    Minor updates on lyrics editor.

--- TomiSoft.LyricsEditor/ViewModels/MainWindowViewModel.cs
+++ TomiSoft.LyricsEditor/ViewModels/MainWindowViewModel.cs
@@ -1,66 +1,37 @@
-﻿using System;
-using System.ComponentModel;
-using System.Windows.Threading;
+﻿using System.ComponentModel;
+using System.Windows.Input;
 using TomiSoft.LyricsEditor.Commands;
+using TomiSoft.MP3Player.Common.MediaInformation;
 using TomiSoft.MP3Player.Communication;
 
 namespace TomiSoft.LyricsEditor.ViewModels {
     class MainWindowViewModel : INotifyPropertyChanged {
-        private readonly ExitApplicationCommand exitApplicationCommand = new ExitApplicationCommand();
-        private readonly NewLyricsCommand newLyricsCommand = new NewLyricsCommand();
-        private readonly OpenLyricsCommand openLyricsCommand = new OpenLyricsCommand();
-        private readonly SaveLyricsCommand saveLyricsCommand = new SaveLyricsCommand();
-        private readonly PlayCommand playCommand;
-        private readonly PauseCommand pauseCommand;
-        private readonly StopCommand stopCommand;
+        private readonly PlayerClient Client;
+        private ISongMetadata songMetadata = new SongMetadata("Sample title", "Sample artist", "Sample album");
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public ExitApplicationCommand ExitApplicationCommand {
-            get {
-                return exitApplicationCommand;
-            }
-        }
-
-        public NewLyricsCommand NewLyricsCommand {
-            get {
-                return newLyricsCommand;
-            }
-        }
-
-        public OpenLyricsCommand OpenLyricsCommand {
-            get {
-                return openLyricsCommand;
-            }
-        }
-
-        public SaveLyricsCommand SaveLyricsCommand {
-            get {
-                return saveLyricsCommand;
-            }
-        }
-
-        public PlayCommand PlayCommand {
-            get {
-                return playCommand;
-            }
-        }
-
-        public PauseCommand PauseCommand {
-            get {
-                return pauseCommand;
-            }
-        }
-
-        public StopCommand StopCommand {
-            get {
-                return stopCommand;
-            }
-        }
+        public ICommand ExitApplicationCommand { get; } = new ExitApplicationCommand();
+        public ICommand NewLyricsCommand       { get; } = new NewLyricsCommand();
+        public ICommand OpenLyricsCommand      { get; } = new OpenLyricsCommand();
+        public ICommand SaveLyricsCommand      { get; } = new SaveLyricsCommand();
+        public ICommand PlayCommand            { get; }
+        public ICommand PauseCommand           { get; }
+        public ICommand StopCommand            { get; }
 
         public PlaybackStatus Playback {
             get;
             private set;
+        }
+
+        public ISongMetadata SongMetadata {
+            get {
+                return this.songMetadata;
+            }
+            set {
+                this.songMetadata = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.SongMetadata)));
+            }
         }
         
         public MainWindowViewModel() {
@@ -68,15 +39,15 @@ namespace TomiSoft.LyricsEditor.ViewModels {
         }
 
         public MainWindowViewModel(PlayerClient Client) {
+            this.Client = Client;
+
+            this.SongMetadata = this.Client.Playlist.CurrentlyPlaying;
+
             this.Playback = new PlaybackStatus(Client);
 
-            this.playCommand = new PlayCommand(Client);
-            this.pauseCommand = new PauseCommand(Client);
-            this.stopCommand = new StopCommand(Client);
-        }
-
-        private void Update(object sender, EventArgs e) {
-            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Playback)));
+            this.PlayCommand  = new PlayCommand(Client);
+            this.PauseCommand = new PauseCommand(Client);
+            this.StopCommand  = new StopCommand(Client);
         }
     }
 }
